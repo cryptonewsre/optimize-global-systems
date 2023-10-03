c1a15c2526408c0d220bc491800583cee387b93a
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-10-03 18:30:06 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,42 +1,17 @@
-    for item in data:
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
-    main()
-
-    data = generate_random_data()
-def generate_random_data():
-    return data
-
-
-if __name__ == "__main__":
-import random}
-        }
-        for (int item : data) {
-        }
-public class RandomDataGenerator {
-
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-
-    }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-    data.forEach(item => {
 main();
-    const data = [];
+        console.log("Random Number: " + item);
         data.push(Math.floor(Math.random() * 100) + 1);
     return data;
 }
+    const data = [];
+function generateRandomData() {
 
-    const data = generateRandomData();
+
+}
     });
 function main() {
     }
+    const data = generateRandomData();
 
-function generateRandomData() {
     for (let i = 0; i < 10; i++) {
-        console.log("Random Number: " + item);
-}
+    data.forEach(item => {
