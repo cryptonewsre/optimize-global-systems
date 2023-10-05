2d15fc202a185a3484dc58c586bd123b097784e0
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-10-06 01:59:41 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,33 +1,52 @@
-function generateRandomData() {
-        console.log("Random Number: " + item);
-}
+    main()
+    for item in data:
+if __name__ == "__main__":
 
-    for (let i = 0; i < 10; i++) {
-    }
-main();
-    return data;
-    const data = generateRandomData();
-    data.forEach(item => {
-    const data = [];
+def generate_random_data():
 
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-}
 
-    return data;
-function main() {
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    return data
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
 
-    const data = [];
-    const data = generateRandomData();
-main();
-        console.log("Random Number: " + item);
-    });
-    data.forEach(item => {
-}
-    for (let i = 0; i < 10; i++) {
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
 
-function generateRandomData() {
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-    }
+        print(f"Random Number: {item}")
+    return data
+
+
+    data = generate_random_data()
+import random
+    for item in data:
+    main()
+def main():
+if __name__ == "__main__":        print(f"Random Number: {item}")
+    return data
+def generate_random_data():
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+if __name__ == "__main__":
+
+    for item in data:
+
+import random
+def main():
+if __name__ == "__main__":
+def generate_random_data():
+
+def main():
+        print(f"Random Number: {item}")
+    for item in data:
+import random
+    return data
+    main()
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
