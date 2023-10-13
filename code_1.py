59f541849433ea544eff5ec9c00afe1166fd208a
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-10-13 18:48:34 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,56 +1,38 @@
-
     for item in data:
-import random
-    data = generate_random_data()
-
-    return data
-def main():
 
     data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-    main()
 
-        print(f"Random Number: {item}")
-def generate_random_data():    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-
-    for item in data:
 import random
-def main():
-
-
-
-    return data
+        print(f"Random Number: {item}")
     main()
+
+if __name__ == "__main__":
+    return data
     data = generate_random_data()
+
 def generate_random_data():
-if __name__ == "__main__":
-    return data
+def main():            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
 
-    main()
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
+}
+        for (int i = 0; i < 10; i++) {
+        }
 
-def main():
-    data = generate_random_data()
-    for item in data:
-if __name__ == "__main__":
-import random
+        for (int item : data) {
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+    }
+            System.out.println("Random Number: " + item);
+        }        }
+        }
+        int[] data = new int[10];
+        for (int item : data) {
 
-def generate_random_data():    return data;
-function generateRandomData() {
-main();
-    const data = generateRandomData();
-    });
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
 
     }
-    const data = [];
-    for (let i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
 }
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
-    data.forEach(item => {
-
-function main() {
-        console.log("Random Number: " + item);
